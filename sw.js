a3fb7b6bfb1a9c07e77fcd852c149fe7f01c24b2
森本拓朗 <ujimattya.mt@gmail.com>
2021-05-31 12:49:21 +0900

    mark-down実装

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+  console.log('ServiceWorker install:', event)
+})
+
+self.addEventListener('activate', (event) => {
+  console.log('ServiceWorker activate:', event)
+})
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch to:', event.request.url)
+  event.respondWith(fetch(event.request))
+})
